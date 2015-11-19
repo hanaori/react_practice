12d297c0674c9066b59a756c20c769d39a6ddbef
hanaori <pa.pi.pu.pe.puwapuwapu@gmail.com>
2015-11-19 22:43:52 +0900

    use react class

--- dist/index.js
+++ dist/index.js
@@ -46,6 +46,8 @@
 
 	/** @jsx React.DOM */"use strict";
 
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
 	var _react = __webpack_require__(1);
 
 	var _react2 = _interopRequireDefault(_react);
@@ -56,17 +58,42 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 	var count = 1;
 	function countUp() {
-	  _reactDom2.default.render(_react2.default.createElement(
-	    "span",
-	    null,
-	    count
-	  ), document.getElementById("kaori"));
+	  _reactDom2.default.render(_react2.default.createElement(Practice, { count: count }), document.getElementById("kaori"));
 	  count++;
 	}
 
 	setInterval(countUp, 1000);
+
+	var Practice = (function (_React$Component) {
+	  _inherits(Practice, _React$Component);
+
+	  function Practice() {
+	    _classCallCheck(this, Practice);
+
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Practice).apply(this, arguments));
+	  }
+
+	  _createClass(Practice, [{
+	    key: "render",
+	    value: function render() {
+	      return _react2.default.createElement(
+	        "span",
+	        null,
+	        this.props.count
+	      );
+	    }
+	  }]);
+
+	  return Practice;
+	})(_react2.default.Component);
 
 /***/ },
 /* 1 */
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 var count = 1;
-function countUp () {
-  ReactDOM.render(<span>{count}</span>, document.getElementById("kaori"))
+function countUp() {
+  ReactDOM.render(<Practice count={count}></Practice>, document.getElementById("kaori"))
   count++;
 }
 
 setInterval(countUp, 1000)
+
+
+class Practice extends React.Component {
+  render() {
+    return (<span>{this.props.count}</span>)
+  }
+}
